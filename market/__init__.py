bc1b0efe1cdc6b6293b67fda7316a741f086cefd
haseeb4339 <haseebullahbj@gmail.com>
2024-12-09 10:11:03 +0500

    create __init__ file

---
+++ market/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+Flask_DEBUG = 1
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///market.db'
+db = SQLAlchemy(app)
+
--- run.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-
-Flask_DEBUG = 1
-app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///market.db'
-db = SQLAlchemy(app)
-
