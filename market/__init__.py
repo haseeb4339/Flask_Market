2d6981a2d0b5ed8a2b28b86f62f3228f68169dce
haseeb4339 <haseebullahbj@gmail.com>
2024-12-09 10:42:15 +0500

    rearrange the files and modified init file to remove db error

--- market/__init__.py
+++ market/__init__.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+import os
 
-Flask_DEBUG = 1
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///market.db'
+# Get the absolute path to the 'market/instance' directory
+instance_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'instance')
+
+# Set the database URI to point to the correct location
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(instance_path, 'market.db')
+
+# Optional: Ensure the instance folder exists
+if not os.path.exists(instance_path):
+    os.makedirs(instance_path)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///market.db'
 db = SQLAlchemy(app)
 
 from market import routes
